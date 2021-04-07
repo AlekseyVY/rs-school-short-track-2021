7edb122c4e364c4c7078fa3e563841bbf9050dad
alekseyvy <alekseyvy@gmail.com>
2021-04-07 20:54:25 +0400

    feature: 15 completed

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,10 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const regexp = new RegExp('@(\\w+).*\\w+');
+  const data = email.match(regexp)[0];
+  return data.slice(1, data.length);
 }
 
 module.exports = getEmailDomain;
