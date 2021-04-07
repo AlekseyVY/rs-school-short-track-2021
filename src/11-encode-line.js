38bdb372c5fa9634193d35535cb52ccd01663388
alekseyvy <alekseyvy@gmail.com>
2021-04-08 02:00:31 +0400

    feature: 11 completed

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,38 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let tmp = null;
+  let count;
+  let out = '';
+  str.split('').map((ele, idx) => {
+    if (tmp === null) {
+      tmp = ele;
+      count = 1;
+    } else if (tmp === ele) {
+      count++;
+      if (idx === str.length - 1) {
+        out += count;
+        out += ele;
+      }
+    } else {
+      if (count <= 1) {
+        out += tmp;
+        tmp = ele;
+        count = 1;
+      } else {
+        out += count;
+        out += tmp;
+        tmp = ele;
+        count = 1;
+      }
+      if (idx === str.length - 1) {
+        out += ele;
+      }
+    }
+    return null;
+  });
+  return out;
 }
 
 module.exports = encodeLine;
